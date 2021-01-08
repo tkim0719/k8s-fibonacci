ff79ae609b0a094c139c6a17cf32ff3d6fa84dd1
tkim0719 <tkim950719@gmail.com>
2021-01-09 03:06:11 +0900

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t tkim0719/multi-client:latest -t tkim0719/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t tkim0719/multi-server:latest -t tkim0719/multi-server:$SHA -f ./server/Dockerfule ./server
+docker build -t tkim0719/multi-worker:latest -t tkim0719/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push tkim0719/multi-client:latest
+docker push tkim0719/multii-server:latest
+docker push tkim0719/multi-worker:latest
+
+docker push tkim0719/multi-client:$SHA
+docker push tkim0719/multii-server:$SHA
+docker push tkim0719/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=tkim0719/multi-server:$SHA
+kubectl set image deployments/client-deployment client=tkim0719/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=tkim0719/multi-worker:$SHA
